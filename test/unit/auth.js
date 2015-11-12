e446e8b1b5172598c734aaf0240a46528e850e2d
tomaszdurka <tomasz@durka.pl>
2015-11-12 13:07:21 +0100

    Add testing frameworks and basic test

---
+++ test/unit/auth.js
@@ -0,0 +1,20 @@
+var assert = require('chai').assert;
+var Auth = require('../../lib/auth');
+var Promise = require('bluebird');
+
+describe('auth', function() {
+
+  it('authorizeConnection', function() {
+    var auth = new Auth();
+    auth.isValidConnection = function(connection) {
+      return false;
+    };
+    auth._authenticate = function(sessionId) {
+      return Promise.resolve();
+    };
+    assert.lengthOf(auth._validConnections, 0);
+    auth.authorizeConnection('foo', 'bar').then(function() {
+      assert.equal(auth._validConnections[0], 'foo');
+    });
+  });
+});
