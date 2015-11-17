e3929b24fc3c9c239179293b8cbaa95ecb3f44ca
Vladimir Finko <vladimir.finko@cargomedia.ch>
2015-11-17 13:22:34 +0100

    test for isValidUser

--- test/unit/cm-api-client.js
+++ test/unit/cm-api-client.js
@@ -1,6 +1,7 @@
 var assert = require('chai').assert;
 var CMApiClient = require('../../lib/cm-api-client');
 var Promise = require('bluebird');
+var sinon = require('sinon');
 
 describe('cm-api-client', function() {
 
@@ -13,4 +14,28 @@ describe('cm-api-client', function() {
     assert.equal(cmHttpClient.baseUrl, baseUrl, 'has proper baseUrl');
     assert.equal(cmHttpClient.apiKey, apiKey, 'has proper apiKey');
   });
+
+  it('call isValidUser', function() {
+    var cmHttpClient = new CMApiClient('http://cm.dev/', 'foo');
+    var userData = {foo: 'bar'};
+    var successStub = sinon.stub(cmHttpClient, '_request', function() {
+      return Promise.resolve(true);
+    });
+
+    cmHttpClient.isValidUser(userData);
+
+    assert.isTrue(successStub.calledOnce);
+    assert.isTrue(successStub.alwaysCalledWithExactly('isValidUser', [userData]));
+
+    successStub.restore();
+    var failStub = sinon.stub(cmHttpClient, '_request', function() {
+      return Promise.resolve(false);
+    });
+    cmHttpClient.isValidUser(userData).catch(function(err) {
+      failStub.threw(err);
+    });
+
+    assert.isTrue(failStub.calledOnce);
+  });
+
 });
