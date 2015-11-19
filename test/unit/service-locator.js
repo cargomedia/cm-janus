dd7b8e99224e1f95da1f3789cfb4a1405410ced3
Vladimir Finko <vladimir.finko@cargomedia.ch>
2015-11-19 14:48:57 +0100

    join tests

--- test/unit/service-locator.js
+++ test/unit/service-locator.js
@@ -8,38 +8,36 @@ describe('serviceLocator', function() {
   var serviceKeyFunction = 'fun';
   var serviceKeyValue = 'val';
 
-  var callCounter = 0;
-  var serviceFunction = function() {
-    callCounter++;
-    return serviceValueFunction;
-  };
-
-  it('registers function', function() {
+  it('registers/gets as a function', function() {
+    var callCounter = 0;
+    var serviceFunction = function() {
+      callCounter++;
+      return serviceValueFunction;
+    };
     serviceLocator.register(serviceKeyFunction, serviceFunction);
 
     assert.strictEqual(serviceLocator.instances[serviceKeyFunction], undefined);
     assert.strictEqual(serviceLocator.serviceRegistrars[serviceKeyFunction], serviceFunction);
+
+    assert.strictEqual(serviceLocator.get(serviceKeyFunction), serviceValueFunction);
+    assert.strictEqual(serviceLocator.instances[serviceKeyFunction], serviceValueFunction);
+
+    assert.strictEqual(serviceLocator.get(serviceKeyFunction), serviceValueFunction);
+    assert.strictEqual(callCounter, 1);
   });
 
-  it('registers value', function() {
+  it('registers/gets as a value', function() {
     serviceLocator.register(serviceKeyValue, serviceValueValue);
 
     assert.strictEqual(serviceLocator.instances[serviceKeyValue], serviceValueValue);
     assert.strictEqual(serviceLocator.serviceRegistrars[serviceKeyValue], undefined);
-  });
-
-  it('gets service from value', function() {
     assert.strictEqual(serviceLocator.get(serviceKeyValue), serviceValueValue);
   });
 
-  it('gets service from function', function() {
-    assert.strictEqual(serviceLocator.get(serviceKeyFunction), serviceValueFunction);
-    assert.strictEqual(serviceLocator.instances[serviceKeyFunction], serviceValueFunction);
-  });
-
-  it('gets function service lazily', function() {
-    assert.strictEqual(serviceLocator.get(serviceKeyFunction), serviceValueFunction);
-    assert.strictEqual(callCounter, 1);
+  after(function() {
+    delete serviceLocator.serviceRegistrars[serviceKeyFunction];
+    delete serviceLocator.instances[serviceKeyFunction];
+    delete serviceLocator.instances[serviceKeyValue];
   });
 
 });
