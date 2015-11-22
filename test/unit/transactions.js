466df6453f3770008bd60cb132a2c7b852447343
tomaszdurka <tomasz@durka.pl>
2015-11-22 20:42:37 +0100

    Add simple unit tests for Transactions

---
+++ test/unit/transactions.js
@@ -0,0 +1,51 @@
+var assert = require('chai').assert;
+require('../helpers/global-error-handler');
+var Transactions = require('../../lib/transactions');
+
+describe('Transactions', function() {
+
+  it('add', function() {
+    var transaction = new Function;
+    var transactions = new Transactions();
+    transactions.add('foo', transaction);
+    assert.strictEqual(transactions.list['foo'], transaction);
+    assert.throw(function() {
+      transactions.add('foo', new Function);
+    });
+  });
+
+  it('find', function() {
+    var transaction = new Function();
+    var transactions = new Transactions();
+    transactions.list = {
+      foo: transaction
+    };
+    assert.strictEqual(transactions.find('foo'), transaction);
+    assert.strictEqual(transactions.find('bar'), null);
+  });
+
+  it('execute', function() {
+    var transaction = new Function();
+    var transactions = new Transactions();
+    transactions.add('foo', transaction);
+    assert.notStrictEqual(transactions.find('foo'), null);
+
+    transactions.execute('foo', {janus: 'ack'});
+    assert.notStrictEqual(transactions.find('foo'), null);
+
+    transactions.execute('foo', {janus: 'event'});
+    assert.strictEqual(transactions.find('foo'), null);
+
+    transactions.execute('bar', null);
+  });
+
+  it('remove', function() {
+    var transactions = new Transactions();
+    transactions.list = {
+      foo: new Function
+    };
+    assert.notStrictEqual(transactions.find('foo'), null);
+    transactions.remove('foo');
+    assert.strictEqual(transactions.find('foo'), null);
+  });
+});
