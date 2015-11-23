5b1acca07da88d8cc4332fdce8df9317f1f34de0
tomaszdurka <tomasz@durka.pl>
2015-11-23 14:34:46 +0100

    Remove command-runner, use spawnSync instead, add tests for cm-application

--- test/unit/cm-application.js
+++ test/unit/cm-application.js
@@ -1,6 +1,5 @@
 var assert = require('chai').assert;
 require('../helpers/global-error-handler');
-var CommandRunner = require('../../lib/command-runner');
 var CmApplication = require('../../lib/cm-application');
 var sinon = require('sinon');
 
@@ -9,25 +8,29 @@ describe('CmApplication', function() {
   this.timeout(1000);
 
   it('runCommand', function() {
-    var commandRunner = new CommandRunner();
-    sinon.stub(commandRunner, 'execute').withArgs('bin/cm', ['packageName', 'action', 'arg1', 'arg2'], {cwd: 'applicationRootPath'}).returns('foo');
-    var cmApplication = new CmApplication('applicationRootPath', commandRunner);
-    assert.strictEqual('foo', cmApplication.runCommand('packageName', 'action', ['arg1', 'arg2']));
+    var cmApplication = new CmApplication('applicationRootPath');
+    var spawnSync = sinon.stub(cmApplication, '_spawnSync').withArgs('bin/cm', ['packageName', 'action', 'arg1', 'arg2'], {cwd: 'applicationRootPath'});
+
+    spawnSync.onCall(0).returns({error: new Error('foo')});
+    assert.throw(function() {
+      cmApplication.runCommand('packageName', 'action', ['arg1', 'arg2']);
+    }, Error);
+
+    spawnSync.onCall(1).returns({output: 'bar'});
+    assert.strictEqual(cmApplication.runCommand('packageName', 'action', ['arg1', 'arg2']), 'bar');
   });
 
   it('importVideoStreamThumbnail', function() {
-    var commandRunner = new CommandRunner();
-    var cmApplication = new CmApplication('applicationRootPath', commandRunner);
+    var cmApplication = new CmApplication('applicationRootPath');
     var runCommand = sinon.stub(cmApplication, 'runCommand');
-    runCommand.withArgs('media-streams', 'import-video-thumbnail', ['streamChannelId', 'thumb']).returns('returned');
-    assert.strictEqual(cmApplication.importVideoStreamThumbnail('streamChannelId', 'thumb'), 'returned');
+    runCommand.withArgs('media-streams', 'import-video-thumbnail', ['streamChannelId', 'thumb']);
+    cmApplication.importVideoStreamThumbnail('streamChannelId', 'thumb');
   });
 
   it('importMediaStreamArchive', function() {
-    var commandRunner = new CommandRunner();
-    var cmApplication = new CmApplication('applicationRootPath', commandRunner);
+    var cmApplication = new CmApplication('applicationRootPath');
     var runCommand = sinon.stub(cmApplication, 'runCommand');
-    runCommand.withArgs('media-streams', 'import-archive', ['streamChannelId', 'archive']).returns('returned');
-    assert.strictEqual(cmApplication.importMediaStreamArchive('streamChannelId', 'archive'), 'returned');
+    runCommand.withArgs('media-streams', 'import-archive', ['streamChannelId', 'archive']);
+    cmApplication.importMediaStreamArchive('streamChannelId', 'archive');
   });
 });
