7e316d8f77414fca5b1f82651295bcf599b1dcdc
Vladimir Finko <vladimir.finko@cargomedia.ch>
2015-11-25 14:18:47 +0100

    add tests for logger

---
+++ test/unit/logger.js
@@ -0,0 +1,79 @@
+var assert = require('chai').assert;
+var sinon = require('sinon');
+var Promise = require('bluebird');
+var path = require('path');
+var fs = require("fs");
+var tmpNameSync = require('tmp').tmpNameSync;
+var rimraf = require('rimraf');
+
+var Logger = require('../../lib/logger');
+
+var globalTmpDir = path.join(__dirname, '/tmp');
+
+describe('Logger', function() {
+  /**
+   * @type {string}
+   */
+  var logFilePath = '';
+
+  /**
+   * @type {WriteStream}
+   */
+  var stream;
+
+  /**
+   * @type {Logger}
+   */
+  var logger;
+
+  before(function(done) {
+    fs.mkdir(globalTmpDir, function() {
+      logFilePath = tmpNameSync({dir: globalTmpDir, postfix: '.log'});
+      done();
+    });
+  });
+
+  after(function(done) {
+    rimraf(globalTmpDir, function(err) {
+      done(err);
+    });
+  });
+
+  beforeEach(function() {
+    stream = fs.createWriteStream(logFilePath, {flags: 'w', defaultEncoding: 'utf8'});
+    logger = new Logger('test');
+    logger.pipe(stream);
+  });
+
+  it('debug numbers', function(done) {
+    logger.debug('foo', 3);
+    stream.end(function() {
+      assert.strictEqual(fs.readFileSync(logFilePath, 'utf8'), 'test debug foo 3\n');
+      done();
+    });
+  });
+
+  it('info arrays', function(done) {
+    logger.info('foo', ['bar', 'baz', 'quux'], 1);
+    stream.end(function() {
+      assert.strictEqual(fs.readFileSync(logFilePath, 'utf8'), 'test info foo ["bar","baz","quux"] 1\n');
+      done();
+    });
+  });
+
+  it('warn objects and arrays', function(done) {
+    logger.warn({foo: [1, 2, 3], bar: {buz: 1}}, ['foo', 'bar', 'buz']);
+    stream.end(function() {
+      assert.strictEqual(fs.readFileSync(logFilePath, 'utf8'), 'test warn {"foo":[1,2,3],"bar":{"buz":1}} ["foo","bar","buz"]\n');
+      done();
+    });
+  });
+
+  it('error Error instance', function(done) {
+    logger.error(new Error("Test error") , 'bar');
+    stream.end(function() {
+      assert.strictEqual(fs.readFileSync(logFilePath, 'utf8'), 'test error [Error: Test error]\n');
+      done();
+    });
+  });
+});
