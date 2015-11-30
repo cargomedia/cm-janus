924366a6e5dc3d95960bd3ac60f3412134ecad48
vogdb <vozhdb@gmail.com>
2015-11-30 14:06:49 +0300

    Remove JanusProxy.stop functionality

--- test/spec/janus-proxy.js
+++ test/spec/janus-proxy.js
@@ -1,10 +1,7 @@
-var _ = require('underscore');
-var assert = require('chai').assert;
 require('../helpers/global-error-handler');
 var WebSocket = require('ws');
 var JanusProxy = require('../../lib/janus-proxy');
 var Logger = require('../../lib/logger');
-var Streams = require('../../lib/streams');
 var serviceLocator = require('../../lib/service-locator');
 
 describe('JanusProxy', function() {
@@ -13,9 +10,6 @@ describe('JanusProxy', function() {
     serviceLocator.reset();
     serviceLocator.register('logger', function() {
       return new Logger();
-    });
-    serviceLocator.register('streams', function() {
-      return new Streams();
     });
   });
 
@@ -30,14 +24,10 @@ describe('JanusProxy', function() {
     var proxy = new JanusProxy(proxyPort, 'http://localhost:' + janusPort);
 
     janusServer.on('connection', function() {
-      assert.equal(_.size(proxy._connections), 1);
-      proxy.stop();
-      assert.equal(_.size(proxy._connections), 0);
       done();
     });
 
     proxy.start();
-    assert.equal(_.size(proxy._connections), 0);
 
     new WebSocket('ws://localhost:' + proxyPort);
   });
