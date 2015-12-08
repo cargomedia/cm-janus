0d830be12f94e98fe724a792aebba9c43ad28d5b
tomaszdurka <tomasz@durka.pl>
2015-12-08 07:58:43 +0100

    Adjust session spec

--- test/spec/janus/session.js
+++ test/spec/janus/session.js
@@ -17,17 +17,17 @@ var Session = require('../../../lib/janus/session');
 var serviceLocator = require('../../../lib/service-locator');
 
 describe('Session', function() {
-  var session, janusConnection;
+  var session, connection;
 
   beforeEach(function() {
     serviceLocator.register('logger', sinon.stub(new Logger));
-    janusConnection = new JanusConnection();
-    session = new Session(janusConnection, 'session-id', 'session-data');
+    connection = new JanusConnection();
+    session = new Session(connection, 'session-id', 'session-data');
     session.pluginRegistry = sinon.createStubInstance(PluginRegistry);
   });
 
   it('should store connection, id and data', function() {
-    expect(session.connection).to.be.equal(janusConnection);
+    expect(session.connection).to.be.equal(connection);
     expect(session.id).to.be.equal('session-id');
     expect(session.data).to.be.equal('session-data');
   });
@@ -37,35 +37,88 @@ describe('Session', function() {
   });
 
   context('when processes "attach" message', function() {
-
     beforeEach(function() {
       var message = {
         janus: 'attach',
         plugin: 'plugin-type',
         token: 'token'
       };
-      sinon.spy(janusConnection.transactions, 'add');
+      sinon.spy(connection.transactions, 'add');
       session.pluginRegistry.instantiatePlugin.returns('plugin-instance');
       session.pluginRegistry.isAllowedPlugin.returns(true);
       session.processMessage(message);
     });
 
     it('transaction should be added', function() {
-      assert(janusConnection.transactions.add.calledOnce);
+      assert(connection.transactions.add.calledOnce);
     });
 
-    it('on successful transaction response should add session', function() {
-      var transactionCallback = janusConnection.transactions.add.firstCall.args[1];
+    it('on successful transaction response should add plugin', function() {
+      var transactionCallback = connection.transactions.add.firstCall.args[1];
       transactionCallback({
         janus: 'success',
         data: {
           id: 'plugin-id'
         }
       });
-      assert(session.pluginRegistry.instantiatePlugin.withArgs('plugin-id', 'plugin-type', janusConnection).calledOnce);
+      assert(session.pluginRegistry.instantiatePlugin.withArgs('plugin-id', 'plugin-type', session).calledOnce);
       expect(_.size(session.plugins)).to.be.equal(1);
       expect(session.plugins).to.have.property('plugin-id');
       expect(session.plugins['plugin-id']).to.be.equal('plugin-instance');
+    });
+  });
+
+  context('when processes "detached" message', function() {
+    beforeEach(function() {
+      var message = {
+        janus: 'detached',
+        sender: 'plugin-id',
+        token: 'token'
+      };
+      sinon.stub(session, '_removePlugin');
+      session.processMessage(message);
+    });
+
+    it('should remove plugin', function() {
+      assert(session._removePlugin.withArgs('plugin-id').calledOnce);
+    });
+  });
+
+  context('when processes "hangup" message', function() {
+    beforeEach(function() {
+      var message = {
+        janus: 'hangup',
+        sender: 'plugin-id',
+        token: 'token'
+      };
+      sinon.stub(session, '_removePlugin');
+      session.processMessage(message);
+    });
+
+    it('should remove plugin', function() {
+      assert(session._removePlugin.withArgs('plugin-id').calledOnce);
+    });
+  });
+
+  context('when removes plugin', function() {
+    beforeEach(function() {
+      session.plugins['plugin-id'] = sinon.createStubInstance(PluginAbstract);
+      session.plugins['other-plugin-id'] = sinon.createStubInstance(PluginAbstract);
+    });
+
+
+    it('should remove it from plugins collection', function() {
+      expect(_.size(session.plugins), 2);
+      expect(session.plugins).to.have.property('plugin-id');
+      session._removePlugin('plugin-id');
+      expect(_.size(session.plugins), 1);
+      expect(session.plugins).to.not.have.property('plugin-id');
+    });
+
+    it('should trigger onRemove', function() {
+      var plugin = session.plugins['plugin-id'];
+      session._removePlugin('plugin-id');
+      assert(plugin.onRemove.calledOnce);
     });
   });
 
