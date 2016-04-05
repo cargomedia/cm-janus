d3cc8878bbe3d77e76d08a487a242a9ad97e5937
Tomasz Durka <tomasz@durka.pl>
2016-04-05 16:37:38 +0200

    Add Context to Stream and Channel

--- test/unit/stream.js
+++ test/unit/stream.js
@@ -4,27 +4,32 @@ var sinon = require('sinon');
 require('../helpers/globals');
 
 var Stream = require('../../lib/stream');
+var Context = require('../../lib/context');
 var PluginAbstract = require('../../lib/janus/plugin/abstract');
 
 describe('Stream', function() {
 
   it('constructor', function() {
     var plugin = sinon.createStubInstance(PluginAbstract);
-    var stream = new Stream('foo', 'channel', plugin);
+    var context = new Context();
+    var stream = new Stream('foo', 'channel', plugin, context);
 
     assert.strictEqual(stream.id, 'foo');
     assert.strictEqual(stream.channel, 'channel');
     assert.strictEqual(stream.plugin, plugin);
+    assert.strictEqual(stream.context, context);
     assert.closeTo(stream.start.getTime(), Date.now(), 1);
   });
 
   it('generate', function() {
     var plugin = sinon.createStubInstance(PluginAbstract);
+    var context = new Context();
 
-    var stream = Stream.generate('channel', plugin);
+    var stream = Stream.generate('channel', plugin, context);
 
     assert.strictEqual(_.isString(stream.id), true);
     assert.strictEqual(stream.channel, 'channel');
     assert.strictEqual(stream.plugin, plugin);
+    assert.strictEqual(stream.context, context);
   });
 });
